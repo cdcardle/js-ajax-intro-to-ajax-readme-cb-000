c7e0e9179422d80b9bd4320bb6677ab8a2ae1479
Chris Cardle <cdcardle@gmail.com>
2018-11-09 15:54:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
 
-function getCommits(el) {
+function showCommits() {
   const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
   document.getElementById('commits').innerHTML = commitsList;
 }
 
